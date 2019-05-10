eff6658fdbb4482a13f428bf00b81b6b97818583
Henrique Tavares <ihenrits@gmail.com>
2019-05-09 21:51:52 -0300

    defaultProps

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
+import Todo from "./components/Todo";
 
 export default class App extends Component {
   render() {
-    return <View style={styles.container} />;
+    return (
+      <View style={styles.container}>
+        <Todo title="Fazer Café" />
+        <Todo />
+      </View>
+    );
   }
 }
 
@@ -12,6 +18,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    backgroundColor: "#f5f5f5"
   }
 });
